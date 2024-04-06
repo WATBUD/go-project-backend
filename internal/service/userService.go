cf8d967c49f369cc7128ebbcf64d19d663e57ea6
水靈 <z23320785@gmail.com>
2024-04-07 03:24:11 +0800

    Regularly updated

--- internal/service/userService.go
+++ internal/service/userService.go
@@ -1,96 +1,49 @@
 package service
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+	"math/rand"
 	"sample/internal/repository/model"
 	"sample/internal/util"
+	"strconv"
 	"time"
 )
 
 type UserService struct {
-	structInterface i_UserRepository
+	structRepository i_UserRepository
 }
 
 type i_UserRepository interface {
-	Signup(data *model.User) error
+	CreateNewUser(data *model.User) (int32, error)
 }
 
-func NewUserService(varName i_UserRepository) *UserService {
+func NewUserService(userRepo i_UserRepository) *UserService {
 	util.PrintLogWithColor("Enter stockService", "#00ffff")
 
 	callBack := UserService{
-		structInterface: varName,
+		structRepository: userRepo,
 	}
 	return &callBack
 }
 
-func (s *UserService) GetDailyClosingQuote() ([]byte, error) {
-	apiURL := "https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?response=json&_=1709118194485"
-	response, err := http.Get(apiURL)
+func (_target *UserService) CreateNewUser(importData interface{}) (int32, error) {
+	randomNum := rand.Intn(10000)
+	randomNumStr := strconv.Itoa(randomNum)
+
+	// 創建一個新的用戶結構
+	user := &model.User{
+		Account:   "example_account" + randomNumStr,
+		Username:  "example_username",
+		Password:  "example_password",
+		Email:     "example@example.com",
+		CreatedAt: time.Now().Format("2006-01-02 15:04:05"),
+		UpdatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	// 調用 structRepository 的 CreateNewUser 方法創建新用戶
+	createdUser, err := _target.structRepository.CreateNewUser(user)
 	if err != nil {
-		return nil, fmt.Errorf("failed to make HTTP request: %v", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode == http.StatusOK {
-		body, err := io.ReadAll(response.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read HTTP response body: %v", err)
-		}
-		return body, nil
+		return 0, err
 	}
 
-	return nil, fmt.Errorf("HTTP request failed with status: %d", response.StatusCode)
-}
-
-func (s *UserService) GetStockMarketOpeningAndClosingDates(requestAllData bool) ([]string, error) {
-
-	type HolidayScheduleResponse struct {
-		Data [][]string `json:"data"`
-	}
-	apiURL := fmt.Sprintf("https://www.twse.com.tw/rwd/zh/holidaySchedule/holidaySchedule?response=json&_=%d", time.Now().Unix())
-
-	response, err := http.Get(apiURL)
-	if err != nil {
-		return nil, fmt.Errorf("failed to make HTTP request: %w", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("HTTP request failed with status: %d", response.StatusCode)
-	}
-
-	var responseBody HolidayScheduleResponse
-	if err := json.NewDecoder(response.Body).Decode(&responseBody); err != nil {
-		return nil, fmt.Errorf("failed to parse JSON response: %w", err)
-	}
-
-	// Extract the dates from the JSON response
-	var dates []string
-	for _, item := range responseBody.Data {
-		dates = append(dates, item[0])
-	}
-
-	return dates, nil
-}
-
-func (s *UserService) GetTheLatestOpeningDate() (string, error) {
-	responseClosingDates, err := s.GetStockMarketOpeningAndClosingDates(false)
-	if err != nil {
-		return "", err
-	}
-
-	currentDate := time.Now()
-	if currentDate.Hour() < 20 {
-		currentDate = currentDate.AddDate(0, 0, -1)
-	}
-
-	for currentDate.Weekday() == time.Saturday || currentDate.Weekday() == time.Sunday || contains(responseClosingDates, currentDate.Format("2006-01-02")) {
-		currentDate = currentDate.AddDate(0, 0, -1)
-	}
-
-	return currentDate.Format("20060102"), nil
+	return createdUser, nil
 }
